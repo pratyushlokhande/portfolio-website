778e01a2f838f273556db3a189ab930bb8f690f6
pratyushlokhande <pratyush3141@gmail.com>
2021-09-12 00:34:39 +0530

    routing and pagination done

---
+++ src/movieState.js
@@ -0,0 +1,73 @@
+export const MovieState = () => {
+  return [
+    {
+      title: "The Athlete",
+      mainImg: "//unsplash.it/720/480",
+      secondaryImg: "//unsplash.it/902",
+      url: "/work/the-athlete",
+      awards: [
+        {
+          title: "Truly A masterpiece",
+          description:
+            "“Lorem Ipsum is simply dummy text of the printing and typesetting industry.”",
+        },
+        {
+          title: "Fresh look on a brutal sport.",
+          description:
+            "“Lorem Ipsum is simply dummy text of the printing and typesetting industry.”",
+        },
+        {
+          title: "It’s okay lmao.",
+          description:
+            "“Lorem Ipsum is simply dummy text of the printing and typesetting industry.”",
+        },
+      ],
+    },
+    {
+      title: "Good Times",
+      mainImg: "//unsplash.it/720/480",
+      url: "/work/good-times",
+      secondaryImg: "//unsplash.it/901",
+      awards: [
+        {
+          title: "Truly A masterpiece",
+          description:
+            "“Lorem Ipsum is simply dummy text of the printing and typesetting industry.”",
+        },
+        {
+          title: "Fresh look on a brutal sport.",
+          description:
+            "“Lorem Ipsum is simply dummy text of the printing and typesetting industry.”",
+        },
+        {
+          title: "It’s okay lmao.",
+          description:
+            "“Lorem Ipsum is simply dummy text of the printing and typesetting industry.”",
+        },
+      ],
+    },
+    {
+      title: "The Racer",
+      mainImg: "//unsplash.it/720/480",
+      url: "/work/the-racer",
+      secondaryImg: "//unsplash.it/900",
+      awards: [
+        {
+          title: "Truly A masterpiece",
+          description:
+            "“Lorem Ipsum is simply dummy text of the printing and typesetting industry.”",
+        },
+        {
+          title: "Fresh look on a brutal sport.",
+          description:
+            "“Lorem Ipsum is simply dummy text of the printing and typesetting industry.”",
+        },
+        {
+          title: "It’s okay lmao.",
+          description:
+            "“Lorem Ipsum is simply dummy text of the printing and typesetting industry.”",
+        },
+      ],
+    },
+  ];
+};
